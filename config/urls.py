fc91ad7d48c1a7d78126e1fcff8410d62412f316
Saidova <saidova.kseniya@mail.ru>
2025-11-04 15:01:39 +0300

    Добавляем главную страницу. Главный URL в config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,28 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+# Простая view для главной страницы
+def home_view(request):
+    html = """
+    <html>
+        <body>
+            <h1>Добро пожаловать в LMS систему!</h1>
+            <p>Доступные эндпоинты:</p>
+            <ul>
+                <li><a href="/admin/">Админка</a></li>
+                <li><a href="/api/courses/">API Курсы</a></li>
+                <li><a href="/api/lessons/">API Уроки</a></li>
+                <li><a href="/api/users/">API Пользователи</a></li>
+            </ul>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
 
 urlpatterns = [
+    path('', home_view, name='home'),  # Добавляем главную страницу
     path('admin/', admin.site.urls),
     path('api/', include('lms.urls')),
 #    path('api/', include('users.urls')),
