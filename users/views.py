8ab5fbaca80beb5c7678d4577d235729ea839138
Saidova <saidova.kseniya@mail.ru>
2025-11-04 16:24:44 +0300

    Обновляем представления пользователей (users/views.py)

--- users/views.py
+++ users/views.py
@@ -1,7 +1,48 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import CustomUser
-from .serializers import UserSerializer
+from .serializers import UserRegisterSerializer, UserProfileSerializer, UserPublicSerializer
+from .permissions import IsProfileOwner
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserRegisterSerializer
+        elif self.action in ['update', 'partial_update']:
+            return UserProfileSerializer
+        elif self.action == 'retrieve':
+            # Для просмотра своего профиля - полная информация, для чужого - публичная
+            if self.get_object() == self.request.user:
+                return UserProfileSerializer
+            else:
+                return UserPublicSerializer
+        return UserPublicSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAuthenticated, IsProfileOwner]
+        return [permission() for permission in self.permission_classes]
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def profile(self, request):
+        """Получить профиль текущего пользователя"""
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['put', 'patch'], permission_classes=[IsAuthenticated, IsProfileOwner])
+    def update_profile(self, request):
+        """Обновить профиль текущего пользователя"""
+        serializer = UserProfileSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
