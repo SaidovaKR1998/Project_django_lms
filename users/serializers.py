493a82a64d51b1739239868d823bceb7582baf9d
Saidova <saidova.kseniya@mail.ru>
2025-11-04 16:11:56 +0300

    Обновляем сериализаторы пользователей (users/serializers.py)

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,35 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from .models import CustomUser
 
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True)
 
-class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password', 'password2', 'first_name', 'last_name', 'phone', 'city')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Пароли не совпадают"})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = CustomUser.objects.create_user(**validated_data)
+        return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'email', 'first_name', 'last_name', 'phone', 'city', 'avatar')
+        read_only_fields = ('email',)
+
+class UserPublicSerializer(serializers.ModelSerializer):
+    """
+    Сериализатор для публичного просмотра профилей
+    """
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'first_name', 'city', 'avatar')
