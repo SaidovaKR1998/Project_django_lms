58c12408729adaa53318caf4b26e6ddbe8184171
Saidova <saidova.kseniya@mail.ru>
2025-11-16 15:22:45 +0300

    Улучшаем ViewSet для курсов с более строгими правами

--- lms/views.py
+++ lms/views.py
@@ -3,7 +3,8 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
-from users.permissions import IsModerator, IsOwner, IsOwnerOrModerator, IsNotModerator
+from users.permissions import IsModerator, IsOwner, IsOwnerOrModerator, IsNotModerator, IsOwnerOrReadOnly
+
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
@@ -16,7 +17,7 @@ class CourseViewSet(viewsets.ModelViewSet):
         """
         Настройка прав доступа для курсов:
         - Создание: только не-модераторы
-        - Просмотр списка и деталей: авторизованные пользователи
+        - Просмотр списка и деталей: авторизованные пользователи (видят только свои + модераторы видят все)
         - Обновление: владелец или модератор
         - Удаление: только владелец
         """
@@ -31,16 +32,23 @@ class CourseViewSet(viewsets.ModelViewSet):
         return [permission() for permission in self.permission_classes]
 
     def get_queryset(self):
+        """
+        Немодераторы видят только свои курсы
+        Модераторы видят все курсы
+        """
         user = self.request.user
+
         if user.groups.filter(name='moderators').exists() or user.is_staff:
-            # Модераторы видят все курсы
+            # Модераторы и админы видят все курсы
             return Course.objects.all()
         else:
             # Обычные пользователи видят только свои курсы
             return Course.objects.filter(owner=user)
 
     def perform_create(self, serializer):
+        """Автоматически привязываем курс к текущему пользователю"""
         serializer.save(owner=self.request.user)
+
 
 class LessonListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = LessonSerializer
@@ -49,19 +57,32 @@ class LessonListCreateAPIView(generics.ListCreateAPIView):
     filterset_fields = ['course', 'owner']
 
     def get_queryset(self):
+        """
+        Немодераторы видят только свои уроки
+        Модераторы видят все уроки
+        """
         user = self.request.user
+
         if user.groups.filter(name='moderators').exists() or user.is_staff:
             return Lesson.objects.all()
         else:
             return Lesson.objects.filter(owner=user)
 
     def get_permissions(self):
+        """
+        Создание уроков разрешено только не-модераторам
+        Просмотр списка - всем авторизованным
+        """
         if self.request.method == 'POST':
             self.permission_classes = [IsAuthenticated, IsNotModerator]
+        else:
+            self.permission_classes = [IsAuthenticated]
         return [permission() for permission in self.permission_classes]
 
     def perform_create(self, serializer):
+        """Автоматически привязываем урок к текущему пользователю"""
         serializer.save(owner=self.request.user)
+
 
 class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.all()
@@ -69,8 +90,27 @@ class LessonRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated, IsOwnerOrModerator]
 
     def get_permissions(self):
+        """
+        Обновление: владелец или модератор
+        Удаление: только владелец
+        Просмотр: авторизованные (с фильтрацией через get_queryset)
+        """
         if self.request.method in ['PUT', 'PATCH']:
             self.permission_classes = [IsAuthenticated, IsOwnerOrModerator]
         elif self.request.method == 'DELETE':
             self.permission_classes = [IsAuthenticated, IsOwner]
+        else:
+            self.permission_classes = [IsAuthenticated]
         return [permission() for permission in self.permission_classes]
+
+    def get_queryset(self):
+        """
+        Немодераторы видят только свои уроки
+        Модераторы видят все уроки
+        """
+        user = self.request.user
+
+        if user.groups.filter(name='moderators').exists() or user.is_staff:
+            return Lesson.objects.all()
+        else:
+            return Lesson.objects.filter(owner=user)
