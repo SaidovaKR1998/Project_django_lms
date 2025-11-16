b8abcbfe0056fcb771a9a1fac78db75efd02cb1c
Saidova <saidova.kseniya@mail.ru>
2025-11-16 15:54:57 +0300

    Откат назад (ошибка)

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,79 +1,23 @@
 from rest_framework import serializers
-from django.contrib.auth.password_validation import validate_password
-from .models import CustomUser, Payment
+from .models import Course, Lesson
 
 
-class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True)
+class LessonSerializer(serializers.ModelSerializer):
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
-        model = CustomUser
-        fields = ('email', 'password', 'password2', 'first_name', 'last_name')
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Пароли не совпадают"})
-        return attrs
-
-    def create(self, validated_data):
-        validated_data.pop('password2')
-        user = CustomUser.objects.create_user(**validated_data)
-        return user
+        model = Lesson
+        fields = '__all__'
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ('id', 'email', 'first_name', 'last_name')
-        read_only_fields = ('email',)
-
-
-class UserPublicSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор для публичного просмотра профилей других пользователей
-    Доступна только общая информация (без пароля, фамилии, истории платежей)
-    """
+class CourseSerializer(serializers.ModelSerializer):
+    lessons = LessonSerializer(many=True, read_only=True)
+    lessons_count = serializers.SerializerMethodField()
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
 
     class Meta:
-        model = CustomUser
-        fields = ('id', 'email', 'first_name')  # Только email и имя, без фамилии
+        model = Course
+        fields = '__all__'
 
-
-class UserPrivateSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор для владельца профиля (полная информация)
-    """
-
-    class Meta:
-        model = CustomUser
-        fields = ('id', 'email', 'first_name', 'last_name')
-        read_only_fields = ('email',)
-
-
-class PaymentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Payment
-        fields = [
-            'id',
-            'user',
-            'payment_date',
-            'paid_course',
-            'paid_lesson',
-            'amount',
-            'payment_method'
-        ]
-        read_only_fields = ['user', 'payment_date']
-
-
-class UserProfileWithPaymentsSerializer(serializers.ModelSerializer):
-    """
-    Сериализатор для профиля владельца с историей платежей
-    Только для владельца профиля
-    """
-    payments = PaymentSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = CustomUser
-        fields = ('id', 'email', 'first_name', 'last_name', 'payments')
-        read_only_fields = ('email',)
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
