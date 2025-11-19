2e8deec583b907ec8c502f0d15d036969b0c55ee
Saidova <saidova.kseniya@mail.ru>
2025-11-19 21:05:53 +0300

    Корректировки - исправляем lms/serializers.py

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from .models import Course, Lesson
-from .validators import validate_youtube_url
+from .models import Course, Lesson, Subscription  # Теперь Subscription доступен
+
 
 class LessonSerializer(serializers.ModelSerializer):
     owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
@@ -8,11 +8,14 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
-        extra_kwargs = {
-            'video_link': {
-                'validators': [validate_youtube_url]
-            }
-        }
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ['id', 'user', 'course', 'subscribed_at']
+        read_only_fields = ['user', 'subscribed_at']
+
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(many=True, read_only=True)
@@ -25,13 +28,11 @@ class CourseSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+    def get_is_subscribed(self, obj):
+        # Признак подписки текущего пользователя на курс
         user = self.context['request'].user
         if user.is_authenticated:
             return obj.subscriptions.filter(user=user).exists()
         return False
-
-class SubscriptionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subscription
-        fields = ['id', 'user', 'course', 'subscribed_at']
-        read_only_fields = ['user', 'subscribed_at']
