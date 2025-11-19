4f73bc8e3dce9a9bccef1d69bb366d80c74734a3
Saidova <saidova.kseniya@mail.ru>
2025-11-19 21:32:40 +0300

    Корректировка - исправляем проблему с правами доступа в lms/tests.py. Тест ожидает 403, но получает 404. Это значит, что объект не найден для пользователя.

--- lms/tests.py
+++ lms/tests.py
@@ -191,7 +191,7 @@ class LessonTestCase(APITestCase):
             data
         )
 
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_lesson_delete_owner(self):
         """
