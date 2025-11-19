745916c07e38ce9d2cea902cac87e02fbe40a787
Saidova <saidova.kseniya@mail.ru>
2025-11-19 21:26:46 +0300

    Корректировка - исправляем users/views.py. Добавляем класс UserViewSet в users/views.py

--- users/views.py
+++ users/views.py
@@ -10,7 +10,51 @@ from .serializers import (
     PaymentSerializer, UserProfileWithPaymentsSerializer, UserPrivateSerializer
 )
 from .permissions import IsProfileOwner
-from lms.paginators import LessonCoursePagination  # Добавьте этот импорт
+from lms.paginators import LessonCoursePagination
+
+# ДОБАВЬТЕ ЭТОТ КЛАСС - он отсутствует!
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserPublicSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UserRegisterSerializer
+        elif self.action in ['update', 'partial_update']:
+            return UserProfileSerializer
+        elif self.action == 'retrieve':
+            if self.get_object() == self.request.user or self.request.user.is_staff:
+                return UserProfileWithPaymentsSerializer
+            else:
+                return UserPublicSerializer
+        elif self.action == 'list':
+            return UserPublicSerializer
+        return UserPublicSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        elif self.action == 'list':
+            self.permission_classes = [IsAuthenticated]
+        elif self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
+            self.permission_classes = [IsAuthenticated, IsProfileOwner | IsAdminUser]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return [permission() for permission in self.permission_classes]
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def profile(self, request):
+        serializer = UserProfileWithPaymentsSerializer(request.user)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['put', 'patch'], permission_classes=[IsAuthenticated, IsProfileOwner])
+    def update_profile(self, request):
+        serializer = UserProfileSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
@@ -28,7 +72,7 @@ class PaymentViewSet(viewsets.ModelViewSet):
     ordering_fields = ['payment_date', 'amount']
     ordering = ['-payment_date']
     permission_classes = [IsAuthenticated]
-    pagination_class = LessonCoursePagination  # Добавьте пагинацию
+    pagination_class = LessonCoursePagination
 
     def get_queryset(self):
         """
